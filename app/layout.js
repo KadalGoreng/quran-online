f4122abf674e8b2309f8d555bf3d588446bd40a4
Revydo <eldodo641@gmail.com>
2023-07-02 22:05:47 +0700

    change icon

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/quran.png" />
+      </head>
       <body className="bg-[#c0e0b1]">
         <Navbar />
         {children}
